38846da97e28b3875ddc6f12a70ef4d1ac143b55
H5Ryder <ryderharris@icloud.com>
2024-06-26 13:31:24 +0900

    Change cors policy attempt 2

--- app.js
+++ app.js
@@ -49,9 +49,12 @@ const limiter = RateLimit({
 app.use(limiter);
 app.use(compression()); // Compress all routes
 
-app.use(cors({
-  origin: '*' // Allow all origins
-}));
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 
 app.use('/', indexRouter);
